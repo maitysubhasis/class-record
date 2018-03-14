729362918ddb58fdd851ed67156361442c9a98bf
holasm <hola.maity@gmail.com>
2018-03-15 03:44:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function Record (allowed) {
+  const { defaults = {} } =  allowed;
+  return (Parent = null) => {
+    return class _Record extends Parent {
+      constructor (object) {
+        super(object);
+        const _defaults = {};
+        // merge the object to this
+        for (const key in defaults) {
+          if (!(key in object)) {
+            _defaults[key] = defaults[key];
+          }
+        }
+        // filter using allowed
+        Object.keys(object).forEach((key) => {
+          key in object
+          ? Object.assign(this, { [key]: object[key] })
+          : '';
+        })
+        Object.assign(this, _defaults);
+      }
+    }
+  }
+}
